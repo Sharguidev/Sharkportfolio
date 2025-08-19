f3d77d33f91c8658a11179caa40cb57ac1bcb322
Sharguidev <guillermoj.obando@gmail.com>
2025-08-19 12:26:08 -0600

    adding prettier to my project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import prettierResponse from '@altano/astro-prettier-response';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [prettierResponse()],
 });
+
